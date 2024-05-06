ca69f6bf9fefb1010f86651f1da70f45a2d8092d
Luis-MolinaC <90949382+Luis-MolinaC@users.noreply.github.com>
2024-05-05 23:45:43 -0500

    Update echo.go

--- builtins/echo.go
+++ builtins/echo.go
@@ -7,9 +7,7 @@ import (
 )
 
 func Echo(w io.Writer, args ...string) error {
-	// Join the arguments into a single string
 	text := strings.Join(args, "Hello world")
-	// Print the text
 	fmt.Fprintln(w, text)
 	return nil
 }
